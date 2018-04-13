89dd021c819eaa8305bf94f7ecb00023a400406c
Ford, Samuel <samuel.ford@disney.com>
2018-04-13 13:45:13 -0400

    remove hello, change to post

--- handler.js
+++ handler.js
@@ -1,38 +1,12 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
 module.exports.whereis = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      users: [
-        {
-          name: "@samuelford",
-          where: [
-            { today: "KP" },
-            { tomorrow: "TD" }
-          ]
-        },
-        {
-          name: "@jdoe",
-          where: [
-            { today: "FA" }
-          ]
-        }        
+      text: "@samuelford is in KP today, @jdoe is in FA",
+      attachments: [
+        { text: JSON.stringify(event) }
       ]
     })
   };
