4d8a47331a37fd6b8d3d4e2b0a84ea05b31f43e0
Ford, Samuel <samuel.ford@disney.com>
2018-04-13 12:03:06 -0400

    basic whereis response

--- handler.js
+++ handler.js
@@ -14,3 +14,28 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.whereis = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      users: [
+        {
+          name: "@samuelford",
+          where: [
+            { today: "KP" },
+            { tomorrow: "TD" }
+          ]
+        },
+        {
+          name: "@jdoe",
+          where: [
+            { today: "FA" }
+          ]
+        }        
+      ]
+    })
+  };
+    
+  callback(null, response);
+};
