2a0bc0e49bc71b4da7ba7a856927519dbdfaecf5
Ford, Samuel <samuel.ford@disney.com>
2018-04-13 14:25:04 -0400

    parse the event from slack

--- handler.js
+++ handler.js
@@ -1,12 +1,18 @@
 'use strict';
 
 module.exports.whereis = (event, context, callback) => {
+  let body = event.body + "";
+  let paramsArray = body.split("&").map(param => param.split("="));
+  
+  let params = {};
+  paramsArray.forEach(param => params[param[0]] = param[1]);
+  
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       text: "@samuelford is in KP today, @jdoe is in FA",
       attachments: [
-        { text: JSON.stringify(event) }
+        { text: params.text }
       ]
     })
   };
